1d238aaadad4453d16d437749fc186db29872290
Tomas Hesse <tomas.hesse@consulence.se>
2020-12-05 11:57:04 +0100

    Hello World

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace SKY.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello World!");
+
+        }
+    }
+}
